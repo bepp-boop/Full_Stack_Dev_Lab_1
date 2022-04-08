398c0c55f6c741f476ba59e9e11414152543f531
Quang M Nguyen <quangteoqs15@gmail.com>
2022-04-08 16:00:18 +0200

    Fix a small bug

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ mongoose
     .connect(process.env.DB_CONNECT, { useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => {
         console.log('Connect to Mongoose server');
-        app.use(express.json());  //Middleware
+        app.use(express.json());  //Middleware 
         app.use(express.static("public"));
         app.use('/api/user', post)
         app.listen(PORT, () => {
